38869c105bba9447b03a9a42bbe18070ab49e318
Ayan Abbas <ayanabbas727@gmail.com>
2024-10-11 15:06:33 +0530

    Add complete functionality

--- app2.py
+++ app2.py
@@ -4,32 +4,45 @@ import pickle
 from nltk.corpus import stopwords
 import re
 from nltk.stem.porter import PorterStemmer
+
 app = Flask(__name__)
+
+# Initialize Porter Stemmer
 ps = PorterStemmer()
+
+# Load pre-trained model and vectorizer
 model = pickle.load(open('model2.pkl', 'rb'))
 tfidfvect = pickle.load(open('tfidfvect2.pkl', 'rb'))
+
 # Build functionalities
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
 def predict(text):
+    # Preprocess the input text
     review = re.sub('[^a-zA-Z]', ' ', text)
     review = review.lower()
     review = review.split()
-    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+    review = [ps.stem(word) for word in review if word not in stopwords.words('english')]
     review = ' '.join(review)
+
+    # Vectorize the preprocessed text and make predictions
     review_vect = tfidfvect.transform([review]).toarray()
     prediction = 'FAKE' if model.predict(review_vect) == 0 else 'REAL'
     return prediction
+
 @app.route('/', methods=['POST'])
 def webapp():
     text = request.form['text']
     prediction = predict(text)
     return render_template('index.html', text=text, result=prediction)
-@app.route('/predict/', methods=['GET','POST'])
+
+@app.route('/predict/', methods=['GET', 'POST'])
 def api():
     text = request.args.get("text")
     prediction = predict(text)
     return jsonify(prediction=prediction)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8000)
